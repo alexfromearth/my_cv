fc2670f7ab63555b74f59832463c3ebe9131b8a4
Alexey Shirokov <shirokovaloff@gmail.com>
2022-02-23 21:11:07 +0300

    Add `Icon` component

---
+++ src/components/Icon/Icon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import { icons } from "./icons";
+
+  type IconName = "phone" | "linkedIn" | "instagram" | "telegram" | "email";
+
+  export let name: IconName;
+  export let width: number | undefined;
+  export let height: number | undefined;
+  let container;
+
+  onMount(() => {
+    container.innerHTML = icons[name]({ width, height })
+  });
+</script>
+
+<div bind:this={container}/>
