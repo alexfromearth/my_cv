8c8a5a8cd03ed303ebdcdd0f4c71ac41a6fe8b20
Alexey Shirokov <shirokovaloff@gmail.com>
2022-02-23 19:00:10 +0300

    Add `Title` component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
-
+	import Board from "./components/Board.svelte";
+	import Section from "./components/Section.svelte";
+	import Title from "./components/Title.svelte";
 </script>
 
 <main>
-	<h1>Aleksey Shirokov CV!</h1>
+	<Board>
+		<div class="info-wrapper">
+			<Title fullName="Aleksey Shirokov" profession="Software Engineer"/>
+		</div>
+			<Section>
+				Aleksey Shirokov CV!
+			</Section>
+	</Board>
 </main>
 
 <style>
+
 	main {
+		position: relative;
 		height: 100vh;
-		background-size: 100% auto;
 		background-image: url('/assets/background.jpg');
 		background-repeat: no-repeat;
+		font-family: "Mr Eaves Reg", serif
 	}
+
+	.info-wrapper {
+		height: 280px;
+		width: 100%;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
 </style>
---
+++ src/components/Title.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let fullName: string;
+  export let profession: string;
+</script>
+
+<div class="brief-info">
+  <div class="full-name">{fullName.toUpperCase()}</div>
+  <div class="profession">{profession}</div>
+</div>
+
+<style>
+    .brief-info {
+        background-color: #0E0E0E;
+        height: 120px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        padding: 0px 50px;
+    }
+
+    .full-name {
+        color: #ffffff;
+        font-size: 35px;
+        letter-spacing: 2px;
+        font-family: "Mr Eaves Bold", serif;
+    }
+
+    .profession {
+        color: #636363;
+        margin-top: 5px;
+    }
+</style>
