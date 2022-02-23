3be6a2d2280c7d7eea82292c550c156077c6ead1
Alexey Shirokov <shirokovaloff@gmail.com>
2022-02-23 18:51:17 +0300

    Add `Board` component

---
+++ src/components/Board.svelte
@@ -0,0 +1,17 @@
+<div class="main-board">
+    <slot></slot>
+</div>
+
+<style>
+    .main-board {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        height: 1200px;
+        width: 900px;
+        background-color: #F2F2F2;
+        border: 1px solid #BABBBD;
+        border-radius: 3px;
+    }
+</style>
