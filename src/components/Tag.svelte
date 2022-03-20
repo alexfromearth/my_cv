77271d6bb1b6e6c4969fc3283462acb98a6e0718
Aleksey Shirokov <aleksey.shirokov@rt.ru>
2022-03-20 23:53:49 +0400

    Add 'Tag' component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<div class="skill-tag">
+  {text}
+</div>
+
+<style>
+    .skill-tag {
+        display: inline-block;
+        background-color: var(--black);
+        color: var(--whitesmoke);
+        font-size: 20px;
+        padding: 7px 10px;
+        border-radius: 10px;
+        margin-right: 10px;
+        margin-bottom: 10px;
+    }
+</style>
